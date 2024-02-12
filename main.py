41326c643f4f566862ae68fed4a67dfa3dfacca4
= <ngaboiris77@gmail.com>
2024-02-12 20:37:53 +0200

    updated main file

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import cv2
+import numpy as np
 import pytesseract
 
-# Path to Tesseract executable (change this if necessary)
-pytesseract.pytesseract.tesseract_cmd = r'/usr/local/bin/tesseract'
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-# Read the image
-image = cv2.imread('image-reading.jpg')
+# load image, grayscale, apply sharpening filter, otsu's threshold 
+image = cv2.imread('ocr.jpg.jpg')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+sharpen = cv2.filter2D(gray, -1, sharpen_kernel)
+thresh = cv2.threshold(sharpen, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-# Convert image to grayscale
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# ocr
+data = pytesseract.image_to_string(thresh, lang='eng', config='--psm 6')
 
-# Perform OCR using Tesseract
-text = pytesseract.image_to_string(gray_image)
+# cv2.imshow('sharpen', sharpen)
+# cv2.imshow('thresh', thresh)
+# cv2.waitKey()
+numbers = ''.join(filter(str.isdigit, data))
 
-# Print the extracted text
-print("Extracted Text:", text.strip())
+# Printing the  extracted numbers
+print("Extracted Numbers:", numbers)
